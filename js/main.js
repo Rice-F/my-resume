61d4fb9955c3d4738e572ccf801baa129cdd8137
Rice-F <2604211051@qq.com>
2017-03-09 00:05:42 +0800

    update nav scroll

--- js/main.js
+++ js/main.js
@@ -8,17 +8,20 @@ $(window).scroll(function(){
 });
 
 //滚动监听使导航条高亮
-$('body').scrollspy({ target: '#navbar-responsive-collapse' });
+$(function(){
+    $('body').scrollspy({ target: '#navbar-responsive-collapse' });
+})
+
 
 //点击导航项滚动效果
 $(function() {
-    $("a.page-scroll").bind('click', function(event) {
+    $("a.page-scroll").click(function(){
         var $anchor = $(this);
         $("html, body").stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
-    });
+    })
 });
 
 //project-hover
