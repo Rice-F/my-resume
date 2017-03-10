15b3f7562fc21c1008ce47330d75f4b869e22cc1
Rice-F <2604211051@qq.com>
2017-03-10 14:46:04 +0800

    update nav scroll

--- js/main.js
+++ js/main.js
@@ -1,40 +1,63 @@
-//导航条固定
-$(window).scroll(function(){
-    if( $(".navbar").offset().top > 150 ){
-        $(".navbar").addClass("navbar-shrink");
-    }else{
-        $(".navbar").removeClass("navbar-shrink");
+;(function(){
+
+    //navbarFixed
+    var navbarFixed = function(){
+        $(window).scroll(function(){
+            if( $(".navbar").offset().top > 150 ){
+                $(".navbar").addClass("navbar-shrink");
+            }else{
+                $(".navbar").removeClass("navbar-shrink");
+            }
+        });
     }
-});
 
-//滚动监听使导航条高亮
-$(function(){
-    $('body').scrollspy({ target: '#navbar-responsive-collapse' });
-})
+    //initNavbarTop
+    var initNavbarTop = function(){
+        if( $(document).scrollTop() > 865 ){
+            $(".navbar").addClass("navbar-shrink");
+        }else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    }
+
+    //scrollSpy
+    var scrollSpy = function(){
+        $('body').scrollspy({ target: '#navbar-responsive-collapse' });
+    }
+
+    //projectHover
+    var projectHover = function(){
+        $('div.project a').mouseenter(function(){
+            $(this).find('.overlay-bgColor').stop().animate({
+                opacity: .8
+            }, 400 );
+            $(this).find('p').stop().animate({
+                opacity: 1
+            }, 400 );
+
+        }).mouseleave(function(){
+            $(this).find('.overlay-bgColor, p').stop().animate({
+                opacity: 0
+            }, 400 );
+        });
+    }
+
+    //Animate pageScroll
+    var pageScroll = function(){
+        $("a.page-scroll").click(function(){
+            var $anchor = $(this);
+            $("html, body").stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    }
 
 
-//点击导航项滚动效果
-$(function() {
-    $("a.page-scroll").click(function(){
-        var $anchor = $(this);
-        $("html, body").stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    })
-});
 
-//project-hover
-$('div.project a').mouseenter(function(){
-    $(this).find('.overlay-bgColor').stop().animate({
-        opacity: .8
-    }, 400 );
-    $(this).find('p').stop().animate({
-        opacity: 1
-    }, 400 );
-
-}).mouseleave(function(){
-    $(this).find('.overlay-bgColor, p').stop().animate({
-        opacity: 0
-    }, 400 );
-});
+    navbarFixed();
+    initNavbarTop();
+    scrollSpy();
+    projectHover();
+    pageScroll();
+}())
